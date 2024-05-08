d3d482eed5b3e3bb8ad108fbba91cd99ba5210a1
chrisvasqm <christianvasquezmelo@gmail.com>
2024-05-08 01:27:46 -0400

    Add environment variables

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,18 @@ import './App.css';
 import React from 'react';
 
 function App() {
+  const fakeUrl = import.meta.env.FAKE_URL;
+  const viteUrl = import.meta.env.VITE_URL;
+
+  console.log(fakeUrl);
+  console.log(viteUrl);
+  
   return (
     <main className='container'>
       <h1>App</h1>
       <p>Description...</p>
+      <p>{import.meta.env.FAKE_URL}</p>
+      <p>{import.meta.env.VITE_URL}</p>
     </main>
   );
 }
